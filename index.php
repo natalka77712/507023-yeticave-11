65adfa2b1ff35fbe73eb2790a23ee0bf510540a8
Евраз <kostousik@mail.ru>
2019-10-29 17:01:17 +0300

    fixup! Выполняет задание по итерации массивов

--- index.php
+++ index.php
@@ -140,10 +140,11 @@ $products = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <!--заполните этот список из массива категорий-->
+        <?php foreach ($categories as $category): ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?= $category ?></a>
             </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
